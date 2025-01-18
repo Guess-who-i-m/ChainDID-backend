d3d0281eca138431e9737e8697f5bb22ed6d5d94
郑祺锐 <1063014455@qq.com>
2025-01-18 10:34:55 +0800

    CreateDID(失败版本)

---
+++ src/main/java/com/edu/hit/chaindidbackend/ChainDidBackendApplication.java
@@ -0,0 +1,13 @@
+package com.edu.hit.chaindidbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChainDidBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ChainDidBackendApplication.class, args);
+    }
+
+}
